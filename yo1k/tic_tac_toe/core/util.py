2f72c72c105d3c2da382f5443c157aa08fa9cb2b
Stanislav Kovalenko <stanislav.l.kovalenko@gmail.com>
2022-01-23 11:42:33 +0300

    Add Python files with game core logic and tests for it (#9)
    * Add Python files with game core logic and tests for it
    
    * Address review concerns
    
    * Address review concerns
    
    * Address review concerns (formatting)
    
    * Address review concerns
    
    * Address review concerns
    
    * Make `__eq__` method in `State` class and reformat tests
    
    * Make `eq` cls decorator
    
    * Add type hints for `eq` cls decorator
    
    * Add type hints for `eq` cls decorator

---
+++ yo1k/tic_tac_toe/core/util.py
@@ -0,0 +1,6 @@
+def eq(cls):
+    """Class decorator providing generic comparison functionality."""
+    def __eq__(self, other) -> bool:
+        return isinstance(other, self.__class__) and self.__dict__ == other.__dict__
+    cls.__eq__ = __eq__
+    return cls
