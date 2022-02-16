26dc6fe2808f7f7f5e575a9b4a5a23550fbb2db4
Stanislav Kovalenko <stanislav.l.kovalenko@gmail.com>
2022-02-16 12:57:44 +0300

    Address review concerns

--- yo1k/tic_tac_toe/test/kernel/test_game.py
+++ yo1k/tic_tac_toe/test/kernel/test_game.py
@@ -190,11 +190,13 @@ class LogicSingleActionTest(unittest.TestCase):
                 step=0)
         self.assertEqual(expected_state, state)
 
-    def test_advance__stop_at__phase_change(self) -> None:
+    def test_advance__stop_at_phase_change(self) -> None:
         state = _new_state()
         expected_required_ready = set(range(len(state.players)))
+        act_queue_px = ListActionQueue(
+                [Action.new_surrender(), Action.new_occupy(Cell(0, 0))])
         Logic((
-                ListActionQueue([Action.new_surrender(), Action.new_occupy(Cell(0, 0))]),
+                act_queue_px,
                 ListActionQueue([]))) \
             .advance(state)
         expected_state = _new_state(
@@ -203,6 +205,7 @@ class LogicSingleActionTest(unittest.TestCase):
                 phase=Phase.OUTROUND,
                 required_ready=expected_required_ready)
         self.assertEqual(expected_state, state)
+        self.assertNotEqual(None, act_queue_px.pop())
 
 
 class LogicMultipleActionsTest(unittest.TestCase):
